bd13a5d4838fee0f3e69cb9cc2bf4f8cfa12252f
Sai Teja <saitejabolla@gmail.com>
2023-08-06 08:35:03 +0530

    setup bodyparser

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const { PORT } = require('./config/serverConfig');
+const bodyParser = require('body-parser');
 
 const startupServer = async ()=>{
     const app = express();
     // const PORT=3000;
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}))
     
     app.listen(PORT,()=>{
         console.log(`listening on ${PORT}`);
